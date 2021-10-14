33f014f2ebaf869d0e166080dcc00652701a5a7b
Gabriel Martinusso <gabrielbramos@outlook.com>
2021-10-14 01:59:28 -0300

    Added server to allowed

--- api/api/settings.py
+++ api/api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '192.168.1.150']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '192.168.1.150', '177.153.58.112']
 
 # Application definition
 
